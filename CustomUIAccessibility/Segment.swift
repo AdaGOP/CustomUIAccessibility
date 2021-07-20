dfedcfd5a472e64a8a42961ec9effa6d9cbded8c
Haryanto Quang <quang.haryanto@gmail.com>
2021-07-21 01:06:17 +0700

    Starter Project initial commit

---
+++ CustomUIAccessibility/Segment.swift
@@ -0,0 +1,17 @@
+//
+//  Segment.swift
+//  CustomUIAccessibility
+//
+//  Created by Haryanto Salim on 21/07/21.
+//
+
+import UIKit
+
+struct Segment {
+
+    // the color of a given segment
+    var color: UIColor
+
+    // the value of a given segment – will be used to automatically calculate a ratio
+    var value: CGFloat
+}
--- CustomUIAccessibility/ViewController.swift
+++ CustomUIAccessibility/ViewController.swift
@@ -3,15 +3,26 @@
 //  CustomUIAccessibility
 //
 //  Created by Haryanto Salim on 21/07/21.
-//
+// https://stackoverflow.com/questions/35752762/making-a-pie-chart-using-core-graphics
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        let pieChartView = PieChartView()
+        pieChartView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 400)
+        pieChartView.segments = [
+            Segment(color: .red, value: 57),
+            Segment(color: .blue, value: 30),
+            Segment(color: .green, value: 25),
+            Segment(color: .yellow, value: 40)
+        ]
+        view.addSubview(pieChartView)
     }
 
 
